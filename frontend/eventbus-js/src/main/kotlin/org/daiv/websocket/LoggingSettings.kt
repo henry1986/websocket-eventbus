2d1825cf923ca920ef288a4866cdc0ca0f680c74
Martin Heinrich <martin.heinrich.dresden@gmx.de>
2019-08-16 08:43:31 +0200

    new helper fct. for Translater Constructor

--- frontend/eventbus-js/src/main/kotlin/org/daiv/websocket/LoggingSettings.kt
+++ frontend/eventbus-js/src/main/kotlin/org/daiv/websocket/LoggingSettings.kt
@@ -12,9 +12,13 @@ data class LoggingFilterRule(val loggerName: String, val level: KotlinLoggingLev
     fun isLoggingEnabled(level: KotlinLoggingLevel) = this.level.ordinal <= level.ordinal
 }
 
-class LoggingSettings{
+class LoggingSettings {
     companion object {
-        fun setLogging(loggerFilterList:List<LoggingFilterRule>, globalLevel: KotlinLoggingLevel){
+        fun setLogging(
+            loggerFilterList: List<LoggingFilterRule>,
+            globalLevel: KotlinLoggingLevel,
+            reformatLoggingName: Boolean = false
+        ) {
             KotlinLoggingConfiguration.LOG_LEVEL = globalLevel
             KotlinLoggingConfiguration.APPENDER = object : Appender {
                 override fun trace(message: Any?) = if (message != null) console.log(message) else {
@@ -51,23 +55,43 @@ class LoggingSettings{
                     return formatMessage(level, loggerName, t = null, msg = msg)
                 }
 
-                override fun formatMessage(level: KotlinLoggingLevel, loggerName: String, t: Throwable?, msg: () -> Any?): Any? {
+                override fun formatMessage(
+                    level: KotlinLoggingLevel,
+                    loggerName: String,
+                    t: Throwable?,
+                    msg: () -> Any?
+                ): Any? {
                     return formatMessage(level, loggerName, marker = null, t = null, msg = msg)
                 }
 
-                override fun formatMessage(level: KotlinLoggingLevel, loggerName: String, marker: Marker?, msg: () -> Any?): Any? {
+                override fun formatMessage(
+                    level: KotlinLoggingLevel,
+                    loggerName: String,
+                    marker: Marker?,
+                    msg: () -> Any?
+                ): Any? {
                     return formatMessage(level, loggerName, marker = null, t = null, msg = msg)
                 }
 
-                override fun formatMessage(level: KotlinLoggingLevel, loggerName: String, marker: Marker?, t: Throwable?, msg: () -> Any?): Any? {
+                private fun String.reformatLoggerName() = split(".").last()
+
+                override fun formatMessage(
+                    level: KotlinLoggingLevel,
+                    loggerName: String,
+                    marker: Marker?,
+                    t: Throwable?,
+                    msg: () -> Any?
+                ): Any? {
                     if (loggerFilterList.any { it.loggerName == loggerName && it.isLoggingEnabled(level) } || loggerFilterList.none { it.loggerName == loggerName }) {
-                        val message = "${level.name}: [$loggerName] ${marker?.getName()?:""} ${msg()}${t.throwableToString()}"
+                        val message =
+                            "${level.name}: [${if (reformatLoggingName) loggerName.reformatLoggerName() else loggerName}] ${marker?.getName()
+                                ?: ""} ${msg()}${t.throwableToString()}"
                         return date(message)
                     }
                     return null
                 }
 
             }
-         }
+        }
     }
 }
